1400563e6adf0b117ba5e45f9cf27bd5d3b7c0b9
aisyahnadhira <aisyahaisyah725@gmail.com>
2023-06-16 13:11:19 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Aisyah Nadhira Salma Maritza" // TODO: replace this
+const studentId = "FE5062928" // TODO: replace this
+
+export { url, studentName, studentId }
